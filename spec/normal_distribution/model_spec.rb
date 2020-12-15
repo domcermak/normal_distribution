72eafb47d42963c781c60b234dd6127e25fbe4c0
Dominik <dominik.cermak@enerfis.cz>
2020-12-16 00:07:52 +0100

    Improve model formatting

--- spec/normal_distribution/model_spec.rb
+++ spec/normal_distribution/model_spec.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
-module NormalDistribution
-  RSpec.describe Model do
-    it 'has a version number' do
-      expect(NormalDistribution::VERSION).not_to be nil
+RSpec.describe NormalDistribution::Model do
+  it 'has a version number' do
+    expect(NormalDistribution::VERSION).not_to be nil
+  end
+
+  context 'with successful evaluation' do
+    let(:data) { [ 1, 2, 2, 3, 3, 3, 4, 4, 5 ] }
+
+    subject { NormalDistribution::Model.new data }
+
+    it 'calculates correctly mean of data' do
+        expect(subject.mean).to eq 3
     end
 
-    context 'with successful evaluation' do
-      let(:data) { [ 1, 2, 2, 3, 3, 3, 4, 4, 5 ] }
+    it 'calculates correctly standard deviation of data' do
+        expect(subject.standard_deviation).to be_within(0.0001).of(1.1547)
+    end
 
-      subject { Model.new data }
+    it 'calculates correctly confidence interval' do
+      bottom, top = subject.confidence_interval(95)
 
-      it 'calculates correctly mean of data' do
-          expect(subject.mean).to eq 3
-      end
-
-      it 'calculates correctly standard deviation of data' do
-          expect(subject.standard_deviation).to be_within(0.0001).of(1.1547)
-      end
-
-      it 'calculates correctly confidence interval' do
-        bottom, top = subject.confidence_interval(95)
-
-        expect(bottom).to be_within(0.0001).of(2.2456)
-        expect(top).to be_within(0.0001).of(3.7543)
-      end
+      expect(bottom).to be_within(0.0001).of(2.2456)
+      expect(top).to be_within(0.0001).of(3.7543)
     end
   end
 end

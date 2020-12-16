2c648d09b78bdec36814a6f83c97f4238e8bafc9
Dominik <dominik.cermak@enerfis.cz>
2020-12-16 16:42:54 +0100

    Add documentation to NormalDistribution::Model

--- lib/normal_distribution/model.rb
+++ lib/normal_distribution/model.rb
@@ -2,11 +2,20 @@
 
 module NormalDistribution
   class Model
+    # :nodoc:
     # attr_reader :mean
+
+    # :nodoc:
     # attr_reader :standard_deviation
 
-    #def initialize(_values); end
+    # Initializes normal distribution from given data
+    # Params:
+    # +values+:: Non-empty array of numbers representing data for normal distribution construction
+    # def initialize(values); end
 
-    #def confidence_interval(_percentage); end
+    # Creates confidence interval for given percentage
+    # Params:
+    # +percentage+:: Number in interval <0, 100>
+    # def confidence_interval(percentage); end
   end
 end
